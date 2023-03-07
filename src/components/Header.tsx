64daf19d86da52b1e36add1ec141921fa1fb31c5
JunSeok <ppu1234@naver.com>
2023-03-08 00:56:53 +0900

    :lipstick: Update the header UI

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,4 @@
-import { BsFillSunFill } from 'react-icons/bs'
+import { BsSun } from 'react-icons/bs'
 import { IoIosNotificationsOutline } from 'react-icons/io'
 import { useNavigate } from 'react-router'
 
@@ -6,14 +6,14 @@ const Header = () => {
     const navigate = useNavigate()
     return (
         <div className='bg-white border-b shadow-sm sticky top-0 z-50 py-3'>
-            <header className='flex justify-between items-center px-3 max-w-6xl mx-auto'>
+            <header className='flex justify-between items-center px-8 max-w-7xl mx-auto'>
                 <div>
                     <img onClick={() => navigate("/")} className="h-16 cursor-pointer" src="/MoneyMindsetLogo.png" alt="logo" />
                 </div>
                 <div>
-                    <ul className='flex space-x-8 items-center'>
+                    <ul className='flex space-x-6 items-center'>
                         <li className="cursor-pointer">
-                            <BsFillSunFill size={35} />
+                            <BsSun size={35} />
                         </li>
                         <li className="cursor-pointer">
                             <IoIosNotificationsOutline size={35} />
