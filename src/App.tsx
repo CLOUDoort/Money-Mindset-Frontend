6aa6fb13c1dd1181676ea36dfdb541261c044f5f
JunSeok <ppu1234@naver.com>
2023-03-25 13:40:21 +0900

    :art: Header component using Outlet

--- src/App.tsx
+++ src/App.tsx
@@ -14,14 +14,15 @@ import { ToastContainer } from 'react-toastify';
 const App = () => {
   return (
     <>
-      <Header />
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/sign-in" element={<SignIn />} />
-        <Route path="/sign-up" element={<SignUp />} />
-        <Route path="/forgot" element={<Forgot />} />
-        <Route path="/profile" element={<Profile />} />
-        <Route path="/money-book" element={<MoneyBook />} />
+        <Route path='/' element={<Header />}>
+          <Route index element={<Home />} />
+          <Route path="/sign-in" element={<SignIn />} />
+          <Route path="/sign-up" element={<SignUp />} />
+          <Route path="/forgot" element={<Forgot />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/money-book" element={<MoneyBook />} />
+        </Route>
       </Routes>
       <ToastContainer />
     </>
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,28 +1,33 @@
+import { Outlet, useNavigate } from 'react-router'
+
 import { BsSun } from 'react-icons/bs'
+import { Fragment } from 'react'
 import { IoIosNotificationsOutline } from 'react-icons/io'
-import { useNavigate } from 'react-router'
 
 const Header = () => {
     const navigate = useNavigate()
     return (
-        <div className='bg-#FBFBFB border-b shadow-sm sticky top-0 z-50 py-3'>
-            <header className='flex justify-between items-center px-12 max-w-7xl mx-auto'>
-                <div>
-                    <img onClick={() => navigate("/")} className="h-14 cursor-pointer" src="/MoneyMindsetLogo.svg" alt="logo" />
-                </div>
-                <div>
-                    <ul className='flex space-x-4 items-center text-sm sm:text-lg'>
-                        <li className="cursor-pointer">
-                            <BsSun size={35} />
-                        </li>
-                        <li className="cursor-pointer">
-                            <IoIosNotificationsOutline size={35} />
-                        </li>
-                        <li onClick={() => navigate("/sign-in")} className="cursor-pointer">Sign-in</li>
-                    </ul>
-                </div>
-            </header>
-        </div>
+        <Fragment>
+            <div className='bg-#FBFBFB border-b shadow-sm sticky top-0 z-50 py-3'>
+                <header className='flex justify-between items-center px-12 max-w-7xl mx-auto'>
+                    <div>
+                        <img onClick={() => navigate("/")} className="h-14 cursor-pointer" src="/MoneyMindsetLogo.svg" alt="logo" />
+                    </div>
+                    <div>
+                        <ul className='flex space-x-4 items-center text-sm sm:text-lg'>
+                            <li className="cursor-pointer">
+                                <BsSun size={35} />
+                            </li>
+                            <li className="cursor-pointer">
+                                <IoIosNotificationsOutline size={35} />
+                            </li>
+                            <li onClick={() => navigate("/sign-in")} className="cursor-pointer">Sign-in</li>
+                        </ul>
+                    </div>
+                </header>
+            </div>
+            <Outlet />
+        </Fragment>
     )
 }
 
