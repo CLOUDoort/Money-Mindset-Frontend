ef468bfc4778d1bff544a123bb7132cec40e475c
JunSeok <ppu1234@naver.com>
2023-04-06 23:12:04 +0900

    :art: Add google login

--- src/components/SignIn/GoogleAuth.tsx
+++ src/components/SignIn/GoogleAuth.tsx
@@ -1,11 +1,8 @@
 import { FcGoogle } from 'react-icons/fc'
 import { Link } from 'react-router-dom';
 
-const GOOGLE_CLIENT_ID = process.env.REACT_APP_GOOGLE_CLIENT_ID
-const GOOGLE_REDIRECT_URI = 'http://localhost:3000/sign-in/google'
-export const googleURL = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${GOOGLE_CLIENT_ID}&response_type=code&redirect_uri=${GOOGLE_REDIRECT_URI}&scope=openid https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile`
-
 const GoogleAuth = () => {
+    const googleURL = 'http://localhost:3714/user/google/login'
     return (
         <Link to={googleURL} className='flex items-center justify-center w-full py-3 font-semibold text-white transition ease-in-out bg-red-600 rounded shadow-md px-7 hover:bg-red-700 hover:shadow-lg active:bg-red-800 duration 150 '>
             <FcGoogle className='mr-2 text-2xl bg-white rounded-full' size={25} />
