d71bf1b5c47f46146f025bb8efc86dd5b0fc6846
JunSeok <ppu1234@naver.com>
2023-04-12 11:20:26 +0900

    :art: Add setting and sign-out

--- src/components/MoneyBook/MoneyBookNav.tsx
+++ src/components/MoneyBook/MoneyBookNav.tsx
@@ -1,21 +1,39 @@
-import { Link, Outlet } from "react-router-dom"
+import { Link, Outlet, useNavigate } from "react-router-dom"
 
 import { AiOutlineBarChart } from 'react-icons/ai'
 import { AiTwotoneCalendar } from 'react-icons/ai'
 import { GiGoalKeeper } from 'react-icons/gi'
 import { ImFileOpenoffice } from 'react-icons/im'
 import { MdDashboard } from 'react-icons/md'
+import { apiInstance } from "../../apis/setting"
+import { toast } from "react-toastify"
+import { useAtomValue } from "jotai"
+import { userIdx } from "../../store/initialState"
+import { IoExitOutline } from 'react-icons/io5'
+import { IoSettingsSharp } from 'react-icons/io5'
 
 const DashNav = () => {
+    const navigate = useNavigate()
+    const idx = useAtomValue(userIdx)
+    const clickLogout = async () => {
+        try {
+            await apiInstance.post(`/user/logout/${idx}`)
+            toast.success("로그아웃!")
+            navigate('/')
+
+        } catch (e: any) {
+            toast.error(e.response)
+        }
+    }
     return (
         <div className="flex h-full">
-            <nav className="sticky left-0 flex flex-col items-center border-r w-60">
-                <ul className="py-4 w-52">
-                    <li className="flex items-center gap-2 p-4 whitespace-nowrap">
+            <nav className="sticky left-0 flex flex-col items-center justify-between border-r w-60">
+                <ul className="flex flex-col items-start justify-center py-4 w-52">
+                    <li className="flex gap-2 p-4 whitespace-nowrap">
                         <MdDashboard size={25} />
                         <Link to='/money-book/dashboard'>Dashboard</Link>
                     </li>
-                    <li className="flex items-center gap-2 p-4 whitespace-nowrap">
+                    <li className="flex gap-2 p-4 whitespace-nowrap">
                         <ImFileOpenoffice size={25} />
                         <Link to='/money-book/expense'>Income & Outcome</Link>
                     </li>
@@ -30,6 +48,16 @@ const DashNav = () => {
                     <li className="flex gap-2 p-4 whitespace-nowrap">
                         <AiOutlineBarChart size={25} />
                         <Link to='/money-book/statistics'>Statistics</Link>
+                    </li>
+                </ul>
+                <ul className="flex flex-col items-start justify-center py-4 w-52">
+                    <li className="flex gap-2 p-4 whitespace-nowrap">
+                        <IoSettingsSharp size={25} />
+                        <button onClick={() => navigate('/money-book/setting')}>Setting</button>
+                    </li>
+                    <li className="flex gap-2 p-4 whitespace-nowrap">
+                        <IoExitOutline size={25} />
+                        <button onClick={clickLogout} >Sign out</button>
                     </li>
                 </ul>
             </nav>
