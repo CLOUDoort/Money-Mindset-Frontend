d362850b0a50bf4dc1e5a05072527952d5c3cc8c
JunSeok <ppu1234@naver.com>
2023-04-14 23:36:42 +0900

    :art: Add icon

--- src/components/MoneyBook/MoneyBookSetting.tsx
+++ src/components/MoneyBook/MoneyBookSetting.tsx
@@ -1,15 +1,27 @@
 import ModifyPassword from "../ModifyPassword"
 import { useState } from "react"
+import { BsSun } from 'react-icons/bs'
+import { IoIosNotificationsOutline } from 'react-icons/io'
 
 const MoneyBookSetting = () => {
     const [modify, setModify] = useState(false)
     const clickModifyPassword = () => setModify(!modify)
     return (
         <>{modify ? <ModifyPassword /> :
-            <><div>
-                setting
-            </div>
-                <button onClick={clickModifyPassword}>Modify password</button></>
+            <>
+                <div>
+                    setting
+                    <button className="border" onClick={clickModifyPassword}>Modify password</button>
+                </div>
+                <div>
+                    <span className="cursor-pointer">
+                        <BsSun size={35} />
+                    </span>
+                    <span className="cursor-pointer">
+                        <IoIosNotificationsOutline size={35} />
+                    </span>
+                </div>
+            </>
         }
         </>
     )
