a0db17b00c8ae2e46f586d316dd874bbe5721b7a
JunSeok <ppu1234@naver.com>
2023-04-24 09:10:05 +0900

    :arrow_up: Add react-query

--- src/App.tsx
+++ src/App.tsx
@@ -10,30 +10,36 @@ import Forgot from "./pages/Forgot";
 import Header from "./components/Header";
 import Home from "./pages/Home";
 import MaginotLine from './pages/MoneyBook/MaginotLine';
+import { QueryClientProvider } from 'react-query';
+import { ReactQueryDevtools } from 'react-query/devtools'
 import Setting from './pages/MoneyBook/Setting';
 import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
 import Statistics from './pages/MoneyBook/Statistics';
 import { ToastContainer } from 'react-toastify';
+import { queryClient } from './react-query/queryClient';
 
 const App = () => {
   return (
     <>
-      <Routes>
-        <Route path='/' element={<Header />}>
-          <Route index element={<Home />} />
-          <Route path="/sign-in" element={<SignIn />} />
-          <Route path="/sign-up" element={<SignUp />} />
-          <Route path="/forgot" element={<Forgot />} />
-        </Route>
-        <Route path="/money-book/setting" element={<Setting />} />
-        <Route path="/money-book/dashboard" element={<Dashboard />} />
-        <Route path="/money-book/expense" element={<Expense />} />
-        <Route path="/money-book/maginot-line" element={<MaginotLine />} />
-        <Route path="/money-book/calendar" element={<Calendar />} />
-        <Route path="/money-book/statistics" element={<Statistics />} />
-      </Routes>
-      <ToastContainer />
+      <QueryClientProvider client={queryClient}>
+        <ReactQueryDevtools />
+        <Routes>
+          <Route path='/' element={<Header />}>
+            <Route index element={<Home />} />
+            <Route path="/sign-in" element={<SignIn />} />
+            <Route path="/sign-up" element={<SignUp />} />
+            <Route path="/forgot" element={<Forgot />} />
+          </Route>
+          <Route path="/money-book/setting" element={<Setting />} />
+          <Route path="/money-book/dashboard" element={<Dashboard />} />
+          <Route path="/money-book/expense" element={<Expense />} />
+          <Route path="/money-book/maginot-line" element={<MaginotLine />} />
+          <Route path="/money-book/calendar" element={<Calendar />} />
+          <Route path="/money-book/statistics" element={<Statistics />} />
+        </Routes>
+        <ToastContainer />
+      </QueryClientProvider>
     </>
   )
 }
---
+++ src/react-query/constants.ts
@@ -0,0 +1,4 @@
+export const queryKeys = {
+    goalData: "goalData",
+    fixedData: "fixedData"
+}
