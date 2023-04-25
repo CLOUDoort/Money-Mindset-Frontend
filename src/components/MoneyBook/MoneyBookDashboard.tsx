c407ff5f95ae2e11074488d2d4951888bd052935
JunSeok <ppu1234@naver.com>
2023-04-25 22:31:36 +0900

    :coffin: remove dead code

--- src/components/MoneyBook/MoneyBookDashboard.tsx
+++ src/components/MoneyBook/MoneyBookDashboard.tsx
@@ -1,40 +1,10 @@
-import { accessToken, userEmail, userIdx } from "../../store/initialState"
-import { useAtom, useAtomValue } from "jotai"
-
 import MoneyBookNav from "./MoneyBookNav"
-import { apiInstance } from "../../apis/setting"
-import { useEffect } from "react"
 
 const MoneyBookDashboard = () => {
-    const [idx, setIdx] = useAtom(userIdx)
-    const [email, setEmail] = useAtom(userEmail)
-    const token = useAtomValue(accessToken)
-    useEffect(() => {
-        const getToken = async () => {
-            try {
-                const userData = await apiInstance.get('user/validate', {
-                    headers: {
-                        Authorization: 'Bearer ' + token
-                    }
-                })
-                const { idx, email } = userData.data
-                setIdx(idx);
-                setEmail(email)
-            }
-            catch (e: any) {
-                console.error(e.response)
-            }
-        }
-        getToken()
-    }, [setEmail, setIdx, token])
     return (
         <div className="flex w-full h-full">
             <MoneyBookNav />
             <div className="flex-1">
-                dashboard
-                idx = {idx}
-                token = {token}
-                email = {email}
             </div>
         </div>
     )
