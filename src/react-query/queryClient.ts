f9dc54fba79590839526a543b58eb166ac0c2d4b
JunSeok <ppu1234@naver.com>
2023-05-11 20:08:33 +0900

    :art: add react-query queryClient setting

--- src/react-query/queryClient.ts
+++ src/react-query/queryClient.ts
@@ -1,3 +1,36 @@
-import { QueryClient } from "react-query";
+import { MutationCache, QueryCache, QueryClient } from "react-query";
 
-export const queryClient = new QueryClient()
+import { toast } from "react-toastify";
+
+function queryErrorHandler(error: unknown): void {
+  // error is type unknown because in js, anything can be an error (e.g. throw(5))
+  const title =
+    error instanceof Error ? error.message : 'error connecting to server';
+
+  // prevent duplicate toasts
+//   toast.closeAll();
+  toast.error(title);
+}
+
+export function generateQueryClient(): QueryClient {
+  return new QueryClient({
+    // 개별 쿼리가 아닌 쿼리 캐시에 handler가 있기 때문에 모든 재시도가 실패한 뒤에 트리거된다.
+    queryCache: new QueryCache({
+        onError: queryErrorHandler
+    }),
+    mutationCache: new MutationCache({
+      onError: queryErrorHandler
+    }),
+    defaultOptions: {
+      queries: {
+        staleTime: 600000,
+        cacheTime: 900000,
+        // refetchOnMount:false,
+        refetchOnReconnect: false,
+        refetchOnWindowFocus: false
+      }
+    }
+})
+}
+
+export const queryClient = generateQueryClient()
