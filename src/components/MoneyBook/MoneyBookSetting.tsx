8af5ad7428d2edd0609058fdb337f4d43f9783e4
JunSeok <ppu1234@naver.com>
2023-05-12 23:56:36 +0900

    :lipstick: update style

--- src/components/MoneyBook/MoneyBookSetting.tsx
+++ src/components/MoneyBook/MoneyBookSetting.tsx
@@ -8,8 +8,8 @@ const MoneyBookSetting = () => {
     const clickModifyPassword = () => setModify(!modify)
     return (
         <>{modify ? <ModifyPassword /> :
-            <div className='flex-1 lg:ml-52 ml-14 bg-[#fbfbfb] min-w-[52rem]'>
-                <div className='flex flex-col items-center justify-center flex-1 p-10'>
+            <div className='flex-1 lg:ml-52 ml-14 bg-[#fbfbfb] min-w-[30rem]'>
+                <div className='flex flex-col items-center justify-center flex-1 p-5 lg:p-10'>
                     <div>
                         setting
                         <button className="border" onClick={clickModifyPassword}>Modify password</button>
--- src/components/MoneyBook/MoneyBookStatistics.tsx
+++ src/components/MoneyBook/MoneyBookStatistics.tsx
@@ -1,7 +1,7 @@
 const MoneyBookStatistics = () => {
     return (
-        <div className="flex-1 lg:ml-52 ml-14 bg-[#fbfbfb] min-w-[52rem]">
-            <div className="flex flex-col items-center justify-center flex-1 p-10">
+        <div className="flex-1 lg:ml-52 ml-14 bg-[#fbfbfb] min-w-[30rem]">
+            <div className="flex flex-col items-center justify-center flex-1 p-5 lg:p-10">
                 statistics
             </div>
         </div>
