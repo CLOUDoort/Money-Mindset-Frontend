a3729603ef01c2d63909d5183795a7a4a7f07165
JunSeok <ppu1234@naver.com>
2023-05-14 17:41:55 +0900

    :lipstick: Update style

--- src/components/InputForm.tsx
+++ src/components/InputForm.tsx
@@ -1,6 +1,6 @@
 const Input = ({ ...otherProps }) => {
     return (
-        <input required autoComplete='off' className="w-full px-4 py-2 mt-2 mb-4 text-xl transition ease-in-out bg-white border-gray-400 rounded h-14" {...otherProps} />
+        <input required autoComplete='off' className="w-full h-12 px-4 py-2 mt-2 mb-4 text-center transition ease-in-out bg-white border-gray-400 rounded" {...otherProps} />
     )
 }
 
--- src/components/MoneyBook/MaginotLine/MoneyBookMaginotLine.tsx
+++ src/components/MoneyBook/MaginotLine/MoneyBookMaginotLine.tsx
@@ -1,15 +1,15 @@
 import MaginotChart from "./MaginotChart"
-import MaginotFixed from "./MaginotFixed"
-import MaginotGoal from "./MaginotGoal"
 import { useAtomValue } from "jotai"
 import { finalMaginot, userAsset } from "../../../store/initialState"
+import MaginotGoal from "./Goal/MaginotGoal"
+import MaginotFixed from "./Fixed/MaginotFixed"
 
 const MoneyBookMaginotLine = () => {
     const asset = useAtomValue(userAsset)
     const finalLine = useAtomValue(finalMaginot)
     return (
-        <div className="flex-1 lg:ml-52 ml-14 bg-[#fbfbfb] min-w-[30rem] flex justify-center items-center">
-            <div className="flex flex-col items-center justify-center w-full p-5 lg:p-10">
+        <div className="flex-1 lg:ml-52 ml-14 bg-[#fbfbfb] min-w-[30rem] w-full flex justify-center items-center">
+            <div className="flex flex-col items-center justify-center lg:w-[75%] w-full p-5 lg:p-10">
                 <div className="my-5 text-3xl font-semibold">현재 자산 : {asset} 원</div>
                 <div className="my-5 text-3xl font-semibold">최종 방어선 : {finalLine} 원</div>
                 <MaginotChart />
