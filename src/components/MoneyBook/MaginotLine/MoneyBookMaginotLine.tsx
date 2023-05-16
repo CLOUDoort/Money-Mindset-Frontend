0b4a7a4d54a41b8b3a00102d4276fabb2a4abe04
JunSeok <ppu1234@naver.com>
2023-05-16 23:19:48 +0900

    :art: Add line data

--- src/components/MoneyBook/MaginotLine/MoneyBookMaginotLine.tsx
+++ src/components/MoneyBook/MaginotLine/MoneyBookMaginotLine.tsx
@@ -5,16 +5,32 @@ import MaginotGoal from "./Goal/MaginotGoal"
 import MaginotFixed from "./Fixed/MaginotFixed"
 import { useGetMaginotLineData } from "../../../react-query/MaginotData/MaginotLineData"
 
+type LineData = {
+    value: number,
+    legend: string
+}
+
 const MoneyBookMaginotLine = () => {
     const asset = useAtomValue(userAsset)
     const finalLine = useAtomValue(finalMaginot)
     const { data } = useGetMaginotLineData()
-    console.log('line', data)
+    let sum = 0;
+    const lineData = data?.data.map((line: LineData, index: number) => {
+        sum += line.value
+        return {
+            "legend": line.legend + ' 방어선',
+            "value": finalLine - sum,
+        }
+    })
+    console.log('lineData', lineData)
     return (
-        <div className="flex-1 lg:ml-52 ml-14 bg-[#fbfbfb] min-w-[30rem] w-full flex justify-center items-center">
-            <div className="flex flex-col items-center justify-center lg:w-[75%] w-full p-5 lg:p-10">
+        <div className="lg:ml-52 ml-14 bg-[#fbfbfb] min-w-[30rem] w-full flex justify-center items-center">
+            <div className="flex flex-col items-center justify-center lg:w-[75%] w-[80%] p-5 lg:p-10">
                 <div className="my-5 text-3xl font-semibold">현재 자산 : {asset} 원</div>
                 <div className="my-5 text-3xl font-semibold">최종 방어선 : {finalLine} 원</div>
+                {lineData && lineData.map((line: LineData) => (
+                    <div className="my-5 text-3xl font-semibold">{line.legend}  : {line.value} 원</div>
+                ))}
                 <MaginotChart />
                 <MaginotGoal />
                 <MaginotFixed />
