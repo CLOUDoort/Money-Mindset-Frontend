44bfca7b85c8f73fae939cc0a71540657bbf0e7c
JunSeok <ppu1234@naver.com>
2023-05-17 19:34:46 +0900

    :art: Add goal_data type

---
+++ src/type/goalData.ts
@@ -0,0 +1,9 @@
+export interface GoalData {
+    amount: number,
+    goal: string,
+    idx: number,
+    line: number,
+    ranking: number,
+    user: {email: string, nickname: string},
+    user_idx: number
+}
