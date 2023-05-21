691f3de06e2ba4627aaf0dde6e584d0c4e5f16ba
JunSeok <ppu1234@naver.com>
2023-05-21 19:42:26 +0900

    :art: Add flow_data modal

--- src/components/MoneyBook/Expense/ExpenseItemList.tsx
+++ src/components/MoneyBook/Expense/ExpenseItemList.tsx
@@ -1,5 +1,7 @@
 import ExpenseItem from "./ExpenseItem"
+import ExpenseItemModal from "./ExpenseItemModal"
 import _ from "lodash"
+import { useState } from "react"
 
 export interface FlowItemType {
     idx: number,
@@ -12,31 +14,41 @@ export interface FlowItemType {
 const ExpenseItemList = ({ data }: any) => {
     const income_list = data.filter((item: FlowItemType) => item.flow_id <= 4)
     const outcome_list = data.filter((item: FlowItemType) => item.flow_id > 4)
+    const [modal, setModal] = useState(false)
+    const [modalData, setModalData] = useState("")
+    const clickModal = (data: string) => {
+        setModal(!modal)
+        setModalData(data)
+    }
     return (
-        <div className="flex items-center justify-between lg:w-[75%] max-w-[70rem] w-[80%] lg:p-10 gap-5">
-            <div className="flex flex-col w-[50%] p-10 my-10 border rounded h-96">
-                <div className="flex items-center justify-between mb-5 font-semibold">
-                    <div className="text-2xl">수입</div>
-                    <div className="cursor-pointer">목록 및 통계</div>
+        <>
+            <div className="flex items-center justify-center lg:w-[75%] max-w-[70rem] w-[80%] gap-3">
+                <div className="flex flex-col w-[50%] p-10 mb-10 border rounded h-[30rem]">
+                    <div className="flex items-center justify-between mb-5 font-semibold">
+                        <div className="text-2xl">수입</div>
+                        <div className="cursor-pointer" onClick={() => clickModal("수입")}>목록 및 통계</div>
+                    </div>
+                    <div className="overflow-y-scroll">
+                        {income_list.map((item: any) => (
+                            <ExpenseItem key={item.idx} data={item} />
+                        ))}
+                    </div>
                 </div>
-                <div className="overflow-y-scroll">
-                    {income_list.map((item: any) => (
-                        <ExpenseItem key={item.idx} data={item} />
-                    ))}
+                <div className="flex flex-col w-[50%] p-10 mb-10 border rounded h-[30rem]">
+                    <div className="flex items-center justify-between mb-5 font-semibold">
+                        <div className="text-2xl">지출</div>
+                        <div className="cursor-pointer" onClick={() => clickModal("지출")}>목록 및 통계</div>
+                    </div>
+                    <div className="overflow-y-scroll">
+                        {outcome_list.map((item: any) => (
+                            <ExpenseItem key={item.idx} data={item} />
+                        ))}
+                    </div>
                 </div>
             </div>
-            <div className="flex flex-col w-[50%] p-10 my-10 border rounded h-96">
-                <div className="flex items-center justify-between mb-5 font-semibold">
-                    <div className="text-2xl">지출</div>
-                    <div className="cursor-pointer">목록 및 통계</div>
-                </div>
-                <div className="overflow-y-scroll">
-                    {outcome_list.map((item: any) => (
-                        <ExpenseItem key={item.idx} data={item} />
-                    ))}
-                </div>
-            </div>
-        </div>
+            {modal && (modalData === "수입" ? <ExpenseItemModal setModal={setModal} item={income_list} modalData={modalData} /> : <ExpenseItemModal setModal={setModal} item={outcome_list} modalData={modalData} />)}
+
+        </>
     )
 }
 
---
+++ src/components/MoneyBook/Expense/ExpenseItemModal.tsx
@@ -0,0 +1,23 @@
+import { useNavigate } from "react-router"
+import ExpenseItem from "./ExpenseItem"
+
+const ExpenseItemModal = ({ setModal, item, modalData }: any) => {
+    const navigate = useNavigate()
+    return (
+        <div className="fixed top-0 left-0 flex items-center justify-center w-full h-full bg-white bg-opacity-50" onClick={() => setModal(false)}>
+            <div onClick={(e) => e.stopPropagation()} className="flex flex-col p-10 z-1000 bg-white lg:w-[60%] max-w-[70rem] w-[60%] lg:ml-52 ml-14 border rounded">
+                <div className="flex items-center justify-between mb-5 font-semibold bg-gray">
+                    <div className="text-2xl">{modalData}</div>
+                    <div className="cursor-pointer" onClick={() => navigate('/money-book/statistics')}>통계</div>
+                </div>
+                <div className="overflow-y-scroll">
+                    {item.map((item: any) => (
+                        <ExpenseItem key={item.idx} data={item} />
+                    ))}
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default ExpenseItemModal
