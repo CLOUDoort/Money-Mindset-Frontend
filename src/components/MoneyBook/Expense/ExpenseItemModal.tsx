b3898fd9f230fb46669c27cd636b1fb6e764e311
JunSeok <ppu1234@naver.com>
2023-05-25 22:45:02 +0900

    :art: Update style

--- src/components/MoneyBook/Expense/ExpenseItemModal.tsx
+++ src/components/MoneyBook/Expense/ExpenseItemModal.tsx
@@ -4,8 +4,8 @@ import ExpenseItem from "./ExpenseItem"
 const ExpenseItemModal = ({ setModal, item, modalData }: any) => {
     const navigate = useNavigate()
     return (
-        <div className="fixed top-0 left-0 flex items-center justify-center w-full h-full bg-white bg-opacity-80" onClick={() => setModal(false)}>
-            <div onClick={(e) => e.stopPropagation()} className="flex flex-col p-10 z-1000 bg-white max-w-[50rem] w-[50%] lg:ml-52 ml-14 border rounded">
+        <div className="fixed top-0 left-0 flex items-center justify-center w-full h-full bg-white bg-opacity-90" onClick={() => setModal(false)}>
+            <div onClick={(e) => e.stopPropagation()} className="flex flex-col p-10 z-1000 bg-white max-w-[50rem] w-[30rem] lg:ml-52 ml-14 border rounded">
                 <div className="flex items-center justify-between mb-5 font-semibold bg-gray">
                     <div className="text-2xl">{modalData}</div>
                     <div className="cursor-pointer" onClick={() => navigate('/money-book/statistics')}>통계</div>
