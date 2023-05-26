857d90f9b94ae0892e89535b9b56529d04e62667
JunSeok <ppu1234@naver.com>
2023-05-26 17:07:14 +0900

    :lipstick: Update style

--- src/components/MoneyBook/Expense/ExpenseItem.tsx
+++ src/components/MoneyBook/Expense/ExpenseItem.tsx
@@ -1,4 +1,4 @@
-import { AiOutlineDelete } from "react-icons/ai"
+import { IoIosRemoveCircleOutline } from "react-icons/io"
 import { useDeleteFlow } from "../../../react-query/Expense/ExpenseFlowData"
 
 const ExpenseItem = ({ data }: any) => {
@@ -13,11 +13,11 @@ const ExpenseItem = ({ data }: any) => {
     const deleteItem = useDeleteFlow()
     return (
         <div className="relative flex items-center p-3text-center border-collapse transition-colors hover:bg-gray-100 group h-[74px] whitespace-nowrap border-b border-b-zinc-500">
-            <div className="flex w-full gap-3 px-5">
+            <div className="flex w-full gap-3 px-10 group">
                 <div className="w-10">{date}</div>
                 <div className="w-16"> {flowName}</div>
                 <div className="flex-1 pr-5 text-right">{amount} 원</div>
-                <AiOutlineDelete onClick={() => deleteItem(idx)} size={25} className="absolute hidden cursor-pointer right-2 group-hover:block" />
+                <IoIosRemoveCircleOutline onClick={() => deleteItem(idx)} size={25} className="absolute hidden transition-colors cursor-pointer right-2 group-hover:block hover:text-red-500" />
             </div>
         </div>
     )
