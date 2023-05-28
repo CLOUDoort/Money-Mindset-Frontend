dfd14cf032bb369e58bc878d9e5e53e75589f73a
JunSeok <ppu1234@naver.com>
2023-05-28 18:32:54 +0900

    :art: Add prefetch chart_data

--- src/react-query/Expense/ExpenseChartData.ts
+++ src/react-query/Expense/ExpenseChartData.ts
@@ -1,8 +1,9 @@
+import { useQuery, useQueryClient } from "react-query";
+
 import { FlowPeriod } from "./ExpenseFlowData";
 import { apiInstance } from "../../apis/setting";
 import { queryKeys } from "../constants";
 import { useAtomValue } from "jotai";
-import { useQuery } from "react-query";
 import { userIdx } from "../../store/initialState";
 
 const getChartData = async (user_idx: number, value: FlowPeriod) => apiInstance.get(`/flow/chart/${user_idx}`, { params: value })
@@ -10,4 +11,10 @@ const getChartData = async (user_idx: number, value: FlowPeriod) => apiInstance.
 export const useGetChartData = (value: FlowPeriod) => {
     const idx = useAtomValue(userIdx)
     return useQuery([queryKeys.chartData], () => getChartData(idx, value))
+}
+
+export const usePrefetchChartData = (value: FlowPeriod) => {
+    const queryClient = useQueryClient()
+    const idx = useAtomValue(userIdx)
+    queryClient.prefetchQuery(queryKeys.chartData, () => getChartData(idx, value))
 }
