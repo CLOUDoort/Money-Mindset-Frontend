138ff862a5ebef3e0617a6d24bb2201b9db09f9f
JunSeok <ppu1234@naver.com>
2023-06-03 13:52:33 +0900

    :art: Add : deleteUSer

--- src/components/MoneyBook/MoneyBookSetting.tsx
+++ src/components/MoneyBook/MoneyBookSetting.tsx
@@ -2,10 +2,31 @@ import { BsSun } from 'react-icons/bs'
 import { IoIosNotificationsOutline } from 'react-icons/io'
 import ModifyPassword from "../ModifyPassword"
 import { useState } from "react"
+import { useAtomValue } from 'jotai'
+import { userIdx } from '../../store/initialState'
+import { apiInstance } from '../../apis/setting'
+import { toast } from 'react-toastify'
+import { useNavigate } from 'react-router'
+import ExpenseItemSkeleton from './Expense/ExpenseItemSkeleton'
 
 const MoneyBookSetting = () => {
     const [modify, setModify] = useState(false)
+    const idx = useAtomValue(userIdx)
+    const navigate = useNavigate()
     const clickModifyPassword = () => setModify(!modify)
+    const deleteUser = async () => {
+        try {
+            console.log('idx', idx)
+            const clickResponse = await apiInstance.delete(`/user/${idx}`)
+            console.log('click', clickResponse)
+            toast.success("탈퇴 성공")
+            navigate('/')
+        }
+        catch (e: any) {
+            console.log(e.response)
+        }
+
+    }
     return (
         <>{modify ? <ModifyPassword /> :
             <div className='flex-1 lg:ml-52 ml-14 bg-[#fbfbfb] min-w-[35rem]'>
@@ -23,8 +44,12 @@ const MoneyBookSetting = () => {
                         </span>
                     </div>
                 </div>
+                <div>
+                    <button onClick={deleteUser} className='text-3xl text-red-600'>회원탈퇴</button>
+                </div>
             </div>
         }
+            <ExpenseItemSkeleton />
         </>
     )
 }
