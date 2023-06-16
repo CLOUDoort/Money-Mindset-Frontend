d95ed527a609c8712d308fe13fef45b636758e88
JunSeok <ppu1234@naver.com>
2023-06-16 23:08:44 +0900

    :art: Add MaginotLine loading

--- src/components/MoneyBook/MaginotLine/MoneyBookMaginotLine.tsx
+++ src/components/MoneyBook/MaginotLine/MoneyBookMaginotLine.tsx
@@ -8,6 +8,7 @@ import MaginotGoal from "./Goal/MaginotGoal"
 import moment from "moment"
 import { useAtomValue } from "jotai"
 import { useGetGoalData } from "../../../react-query/MaginotData/MaginotGoalData"
+import Loading from "../../Loading"
 
 export type LineData = {
     legend: string,
@@ -21,7 +22,7 @@ export const end_date_string = new Date(today.getFullYear(), today.getMonth() + 
 const MoneyBookMaginotLine = () => {
     const asset = useAtomValue(userAsset)
     const finalLine = useAtomValue(finalMaginot)
-    const { data } = useGetGoalData()
+    const { data, isLoading } = useGetGoalData()
     const [line, setLine] = useState([])
 
     useEffect(() => {
@@ -43,23 +44,25 @@ const MoneyBookMaginotLine = () => {
         }
     }, [data?.data, finalLine])
     return (
-        <div className="lg:ml-52 ml-14 bg-[#fbfbfb] min-w-[45rem] w-full flex justify-center items-center">
-            <div className="flex flex-col items-center justify-center lg:w-[75%] max-w-[70rem] w-[80%] lg:p-5">
-                <div className='flex items-center w-full gap-3 my-10 text-2xl font-semibold'>
-                    <div>{start_date_string} ~</div>
-                    <div>{end_date_string}</div>
+        <>
+            {(!isLoading && line) ? <div className="lg:ml-52 ml-14 bg-[#fbfbfb] min-w-[45rem] w-full flex justify-center items-center">
+                <div className="flex flex-col items-center justify-center lg:w-[75%] max-w-[70rem] w-[80%] lg:p-5">
+                    <div className='flex items-center w-full gap-3 my-10 text-2xl font-semibold'>
+                        <div>{start_date_string} ~</div>
+                        <div>{end_date_string}</div>
+                    </div>
+                    <div className="my-5 text-3xl font-semibold">현재 자산 : {asset} 원</div>
+                    {line && line?.map((line: LineData) => {
+                        return (
+                            <div key={line.value} className="my-5 text-2xl font-semibold">{line.legend} 방어선 : {line.value} 원</div>
+                        )
+                    })}
+                    {line && <MaginotChart line={line} />}
+                    <MaginotGoal />
+                    <MaginotFixed />
                 </div>
-                <div className="my-5 text-3xl font-semibold">현재 자산 : {asset} 원</div>
-                {line && line?.map((line: LineData) => {
-                    return (
-                        <div key={line.value} className="my-5 text-2xl font-semibold">{line.legend} 방어선 : {line.value} 원</div>
-                    )
-                })}
-                {line && <MaginotChart line={line} />}
-                <MaginotGoal />
-                <MaginotFixed />
-            </div>
-        </div>
+            </div> : <Loading />}
+        </>
     )
 }
 
