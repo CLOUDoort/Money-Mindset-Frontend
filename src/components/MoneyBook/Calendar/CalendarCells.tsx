f75a0d73acaeb6dfa23d0a788066dde513e8e15c
JunSeok <ppu1234@naver.com>
2023-07-02 00:18:14 +0900

    :art: Calendar modal

--- src/components/MoneyBook/Calendar/CalendarCells.tsx
+++ src/components/MoneyBook/Calendar/CalendarCells.tsx
@@ -1,6 +1,9 @@
 import { addDays, endOfMonth, endOfWeek, format, startOfMonth, startOfWeek } from "date-fns"
 
-const CalendarCells = ({ currentMonth, onDateClick, clickModal }: { currentMonth: Date, onDateClick: (day: Date) => void, clickModal: () => void }) => {
+import CalendarModal from "./CalendarModal"
+import { useState } from "react"
+
+const CalendarCells = ({ currentMonth, onDateClick, selectedDate }: { currentMonth: Date, onDateClick: (day: Date) => void, selectedDate: string }) => {
     const monthStart = startOfMonth(currentMonth)
     const monthEnd = endOfMonth(monthStart)
     const startDate = startOfWeek(monthStart)
@@ -12,16 +15,20 @@ const CalendarCells = ({ currentMonth, onDateClick, clickModal }: { currentMonth
     let day = startDate
     let formattedDate = ''
 
+    const [click, setModal] = useState(false)
+    const clickModal = () => setModal(!click)
+
     while (day <= endDate) {
         for (let i = 0; i < 7; i++) {
-            formattedDate = format(day, 'd')
+            formattedDate = format(day, 'yyyy-MM-dd')
             const cloneDay = day
             days.push(
-                <div className={`w-full gap-1 border rounded pb-[12%] text-end ${format(currentMonth, 'M') !== format(day, 'M') ? 'bg-black/10' : 'cursor-pointer'}`} key={formattedDate} onClick={() => {
+                <div className={`w-full relative gap-1 border rounded pb-[12%] text-end ${format(currentMonth, 'M') !== format(day, 'M') ? 'bg-black/10' : 'cursor-pointer'}`} key={formattedDate} onClick={() => {
                     clickModal()
                     onDateClick(cloneDay)
                 }} >
-                    <span className={`p-1 px-2 ${format(today, 'yyyy-MM-dd') === format(day, 'yyyy-MM-dd') && "text-red-600 font-bold"}`}>{formattedDate}</span>
+                    <span className={`p-1 px-2 ${format(today, 'yyyy-MM-dd') === format(day, 'yyyy-MM-dd') && "text-red-600 font-bold"}`}>{format(day, 'd')}</span>
+                    {click && formattedDate === selectedDate && <CalendarModal clickModal={clickModal} select={selectedDate} />}
                 </div>
             )
             day = addDays(day, 1)
@@ -35,7 +42,7 @@ const CalendarCells = ({ currentMonth, onDateClick, clickModal }: { currentMonth
     }
 
     return (
-        <div className="flex items-center flex-col justify-between min-w-[45rem] w-[50rem] lg:w-[51rem] border border-t-0 border-collapse p-1 gap-1 rounded-t-none">
+        <div className="flex items-center flex-col justify-between min-w-[45rem] w-[50rem] lg:w-[51rem] border border-t-0 border-collapse p-1 gap-1 rounded rounded-t-none">
             {rows}
         </div>
     )
--- src/components/MoneyBook/Calendar/CalendarModal.tsx
+++ src/components/MoneyBook/Calendar/CalendarModal.tsx
@@ -2,7 +2,7 @@ const CalendarModal = ({ select, clickModal }: { select: string, clickModal: () 
     return (
         // <div className="lg:ml-36 ml-14 min-w-[47rem] flex-col fixed top-0 left-0 flex items-center justify-center w-full h-full z-50">
         <div className="bg-slate-300 absolute border z-50 left-28 top-0 rounded w-[10rem] h-[10rem]">
-            <div className="absolute rotate-45 w-7 h-7 bg-slate-300 -left-2 top-5"></div>
+            <div className="absolute rotate-45 rounded-l w-7 h-7 bg-slate-300 -left-2 top-5"></div>
             {select}
         </div>
         // </div>
