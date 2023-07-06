eeaf52a5b193eea95087fe93ae8dc6e85dd8ffc3
JunSeok <ppu1234@naver.com>
2023-07-06 23:54:23 +0900

    :adhesive_bandage: Simple fix

--- src/components/MoneyBook/Calendar/MoneyBookCalendar.tsx
+++ src/components/MoneyBook/Calendar/MoneyBookCalendar.tsx
@@ -15,8 +15,8 @@ const MoneyBookCalendar = () => {
         setSelectedDate(format(day, "yyyy-MM-dd"))
     }
     return (
-        <div className="lg:ml-52 ml-14 relative bg-[#fbfbfb] min-w-[50rem] h-full w-full flex flex-col justify-center items-center">
-            <div className="min-h-[55rem]">
+        <div className="lg:ml-52 ml-14 bg-[#fbfbfb] min-w-[55rem] w-full flex justify-center items-center my-20">
+            <div className="flex flex-col items-center justify-center max-w-[55rem] min-h-[55rem]">
                 <CalendarHeader currentMonth={currentMonth} preMonth={preMonth} nextMonth={nextMonth} goToday={goToday} />
                 <CalendarDays />
                 <CalendarCells currentMonth={currentMonth} onDateClick={onDateClick} selectedDate={selectedDate} />
