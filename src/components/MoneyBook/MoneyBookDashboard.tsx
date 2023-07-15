0ad6047a8b0137bf88582986122ef8a7130b1ec8
JunSeok <ppu1234@naver.com>
2023-07-15 23:55:11 +0900

    :art: Add dashboard

--- src/components/MoneyBook/MoneyBookDashboard.tsx
+++ src/components/MoneyBook/MoneyBookDashboard.tsx
@@ -1,9 +1,18 @@
+import { useGetFlowData } from "../../react-query/Expense/ExpenseFlowData"
+import ExpenseItemList from "./Expense/ExpenseItemList"
+import { end_date_string, start_date_string } from "./MaginotLine/MoneyBookMaginotLine"
+import { end_date, start_date } from "./MoneyBookNav"
+
 const MoneyBookDashboard = () => {
+    const { data: flow_data } = useGetFlowData({ start_date, end_date })
     return (
-        <div className="flex-1 lg:ml-52 ml-14 bg-[#fbfbfb] min-w-[35rem]">
-            <div className="flex flex-col items-center justify-center lg:w-[75%] max-w-[70rem] w-[80%] lg:p-5">
+        <div className="lg:ml-52 ml-14 relative bg-[#fbfbfb] min-w-[47rem] w-full flex flex-col justify-center gap-10 items-center">
+            <div className='flex items-center lg:w-[75%] mt-10 max-w-[70rem] w-[80%] text-2xl gap-3 font-semibold' >
+                <div>{start_date_string} ~</div>
+                <div>{end_date_string}</div>
             </div>
-        </div>
+            {flow_data && <ExpenseItemList data={flow_data?.data} />}
+        </div >
     )
 }
 
