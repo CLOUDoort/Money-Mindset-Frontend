05a2e4251f746837eba5e0b40d9f040e37534409
JunSeok <ppu1234@naver.com>
2023-07-25 15:04:35 +0900

    :art: delete income&outcome data

--- src/components/MoneyBook/Expense/ExpenseModal.tsx
+++ src/components/MoneyBook/Expense/ExpenseModal.tsx
@@ -1,15 +1,32 @@
+import { FlowDataType, graph_color } from "../../../type"
 import { end_date, start_date } from "../MoneyBookNav"
 import { useEffect, useState } from "react"
-import { useGetExpenseStatisticsIncomeData, useGetExpenseStatisticsOutcomeData } from "../../../react-query/Expense/ExpenseStatisticsData"
 
 import ExpenseModalItem from "./ExpenseModalItem"
 import ExpenseStatistics from "./ExpenseStatisticsModal"
 import { MdOutlineSpeakerNotesOff } from "react-icons/md"
+import { useGetFlowData } from "../../../react-query/Expense/ExpenseFlowData"
 
 const ExpenseModal = ({ setModal, item, modalData }: any) => {
     const [state, setState] = useState(false)
-    const { data: in_static } = useGetExpenseStatisticsIncomeData({ start_date, end_date, flow_type: 0 })
-    const { data: out_static } = useGetExpenseStatisticsOutcomeData({ start_date, end_date, flow_type: 1 })
+    const { data: flow_data } = useGetFlowData({ start_date, end_date })
+
+    const in_expense = flow_data?.data?.filter((item: FlowDataType) => { return item.flow_id <= 4 })?.map((item: FlowDataType, index: number) => {
+        return {
+            id: item.idx,
+            label: item.flowName,
+            value: item.amount,
+            color: graph_color[index]
+        }
+    })
+    const out_expense = flow_data?.data?.filter((item: FlowDataType) => { return item.flow_id > 4 })?.map((item: FlowDataType, index: number) => {
+        return {
+            id: item.idx,
+            label: item.flowName,
+            value: item.amount,
+            color: graph_color[index]
+        }
+    })
 
     useEffect(() => {
         document.body.style.cssText = `
@@ -27,7 +44,7 @@ const ExpenseModal = ({ setModal, item, modalData }: any) => {
     return (
         <div className="min-w-[47rem] flex-col fixed top-0 left-0 flex items-center justify-center w-full h-full bg-white bg-opacity-90 z-50" onClick={() => setModal(false)}>
             <div onClick={(e) => e.stopPropagation()} className="flex flex-col bg-white w-[50rem] border border-black rounded h-[40rem]">
-                {state ? <ExpenseStatistics click={clickStatistics} data={modalData === "수입" ? in_static : out_static} /> : <>
+                {state ? <ExpenseStatistics click={clickStatistics} data={modalData === "수입" ? in_expense : out_expense} /> : <>
                     <div className="flex items-center justify-between pt-10 pb-5 font-semibold px-14 bg-gray">
                         <div className="text-2xl">{modalData}</div>
                         <div className="cursor-pointer" onClick={clickStatistics}>통계</div>
