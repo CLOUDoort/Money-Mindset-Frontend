1460c00e6d7a85d7b737708ead5b3c6a1b2d8acf
JunSeok <ppu1234@naver.com>
2023-07-26 16:55:54 +0900

    :art: Update text

--- src/components/MoneyBook/MaginotLine/MoneyBookMaginotLine.tsx
+++ src/components/MoneyBook/MaginotLine/MoneyBookMaginotLine.tsx
@@ -38,7 +38,7 @@ const MoneyBookMaginotLine = () => {
                 {lineData ?
                     <div className="grid w-full grid-cols-2 gap-3 my-10 text-2xl">
                         {lineData?.map((item: LineData) => (
-                            <div key={item.idx} className={`flex flex-col items-center ${asset?.data?.userExpense > item.value ? "bg-red-300" : "bg-green-300"} text-white justify-center font-semibold w-full h-[10rem] border rounded-3xl`}>
+                            <div key={item.idx} className={`flex flex-col items-center ${asset?.data?.userExpense > item.value ? "bg-red-300" : "bg-green-300"} justify-center font-semibold w-full h-[10rem] border rounded-3xl`}>
                                 <div>
                                     {item.value - asset?.data?.userExpense < 0 ? "파괴" : item.value - asset?.data?.userExpense + "원 사용가능"}
                                     <div>{item.legend} 방어선</div>
