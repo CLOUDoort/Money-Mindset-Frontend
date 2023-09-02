f1c93ecd6bed66871fa9ba03853d742c807c6d14
JunSeok <ppu1234@naver.com>
2023-09-03 02:30:34 +0900

    :art: Update InputForm

--- src/components/InputForm.tsx
+++ src/components/InputForm.tsx
@@ -1,7 +1,11 @@
+import { twMerge } from "tailwind-merge"
+
 const Input = ({ ...otherProps }) => {
+    const { styleProp } = otherProps
+    const className = twMerge("w-full h-12 px-4 py-2 my-1 mb-1 transition ease-in-out bg-white border-gray-400 rounded", styleProp)
     const registerInput = otherProps?.register
     return (
-        <input autoComplete='off' className="w-full h-12 px-4 py-2 my-1 mb-1 transition ease-in-out bg-white border-gray-400 rounded" {...otherProps} {...registerInput} />
+        <input autoComplete='off' required className={className} {...otherProps} {...registerInput} />
     )
 }
 
